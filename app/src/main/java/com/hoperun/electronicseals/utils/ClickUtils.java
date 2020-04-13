6f9d3d67efb750fef1cc35ec7eaec65a34e6dac9
gongyu <1026620250@qq.com>
2020-04-13 19:18:57 +0800

    click

--- app/src/main/java/com/hoperun/electronicseals/utils/ClickUtils.java
+++ app/src/main/java/com/hoperun/electronicseals/utils/ClickUtils.java
@@ -1,16 +1,19 @@
 package com.hoperun.electronicseals.utils;
 
+import android.util.Log;
+
 public class ClickUtils {
     private static final int MIN_CLICK_DELAY_TIME = 2000;
-    private static long lastClickTime;
+    private static long lastClickTime = 0L;
 
     public static boolean isFastClick() {
-        boolean flag = false;
+        boolean flag = true;
         long curClickTime = System.currentTimeMillis();
         if ((curClickTime - lastClickTime) >= MIN_CLICK_DELAY_TIME) {
-            flag = true;
+            flag = false;
         }
         lastClickTime = curClickTime;
+        Log.e("gongyu", "sw  " + flag);
         return flag;
     }
 }
