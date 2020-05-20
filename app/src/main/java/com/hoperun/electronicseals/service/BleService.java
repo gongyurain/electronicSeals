d67c163d53ee4b6eaece908c4b62221af81d9dbc
gongyu <1026620250@qq.com>
2020-05-20 20:11:12 +0800

    BLE: update ui; add notify

--- app/src/main/java/com/hoperun/electronicseals/service/BleService.java
+++ app/src/main/java/com/hoperun/electronicseals/service/BleService.java
@@ -1,33 +1,18 @@
 package com.hoperun.electronicseals.service;
 
-import android.app.Service;
 import android.content.Context;
-import android.content.Intent;
-import android.os.Binder;
 import android.os.Handler;
-import android.os.IBinder;
 import android.util.Log;
-
+import android.widget.Toast;
 import com.hoperun.electronicseals.bean.DeviceEventResp;
 import com.inuker.bluetooth.library.BluetoothClient;
-import com.inuker.bluetooth.library.connect.response.BleReadResponse;
-import com.inuker.bluetooth.library.connect.response.BleWriteResponse;
+import com.inuker.bluetooth.library.connect.response.BleNotifyResponse;
 import com.inuker.bluetooth.library.model.BleGattProfile;
 import com.inuker.bluetooth.library.search.SearchResult;
-
-import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
-import org.eclipse.paho.client.mqttv3.MqttCallback;
-import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
-import org.eclipse.paho.client.mqttv3.MqttException;
-import org.eclipse.paho.client.mqttv3.MqttMessage;
-import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import static com.inuker.bluetooth.library.Code.REQUEST_SUCCESS;
 
@@ -40,11 +25,13 @@ public class BleService {
         return instance;
     }
 
+    Context context;
     BluetoothClient mClient;
     ArrayList<BleGattProfile> profiles = new ArrayList<>();
 
     public void init(Context context) {
         if (mClient == null) {
+            this.context = context;
             mClient = new BluetoothClient(context);
         }
     }
@@ -81,13 +68,12 @@ public class BleService {
 
     SearchResult current;
     BleGattProfile profile;
-    boolean syncing = false;
 
-    private UUID getUUID(String number){
+    private UUID getUUID(String number) {
         return UUID.fromString(String.format("0000%4s-0000-1000-8000-00805F9B34FB", number));
     }
 
-    Runnable syncRunnable = () -> mClient.read(
+    Runnable readRunnable = () -> mClient.read(
             current.getAddress(), getUUID("FFE1"), getUUID("C001"),
 //            current.getAddress(), getUUID("1800"), getUUID("2A00"),
             (code, data) -> {
@@ -97,31 +83,30 @@ public class BleService {
                     Log.d(TAG, "convert to event:" + event);
                     eventList.add(event);
                     notifyListener(event);
-                    new Handler().postDelayed(this::doSycn, 1000);
                 }
             });
 
-    public void sycnData(SearchResult result, BleGattProfile profile) {
-        Log.d(TAG, "sycnData");
+    public void readNotify(SearchResult result, BleGattProfile profile) {
+        Log.d(TAG, "readNotify");
         current = result;
         this.profile = profile;
-        syncing = true;
-        doSycn();
-    }
+        mClient.notify(
+//                result.getAddress(), getUUID("FFE1"), getUUID("C001"),
+                result.getAddress(), getUUID("180D"), getUUID("2A37"),
+                new BleNotifyResponse() {
+                    @Override
+                    public void onNotify(UUID service, UUID character, byte[] value) {
+                        Log.d(TAG, "onNotify value: " + new String(value));
+                        Toast.makeText(context, new String(value), Toast.LENGTH_SHORT).show();
+                        DeviceEventResp event = new DeviceEventResp(); //TODO parse data to event
+                        Log.d(TAG, "convert to event:" + event);
+                        eventList.add(event);
+                        notifyListener(event);
+                    }
 
-    private void doSycn() {
-        if (syncing) {
-            Log.d(TAG, "post sync runnable for read");
-            new Handler().post(syncRunnable);
-            syncing = false;
-        }
-//        mClient.write(current.getAddress(), UUID.fromString("0xFFE1"), UUID.fromString(""), "".getBytes(), new BleWriteResponse() {
-//            @Override
-//            public void onResponse(int code) {
-//                if (code == REQUEST_SUCCESS) {
-//
-//                }
-//            }
-//        });
+                    @Override
+                    public void onResponse(int code) {
+                    }
+                });
     }
 }
